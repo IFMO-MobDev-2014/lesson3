79c3f0f029bda4c3ef2746656a644fad8300e870
Ignat Loskutov <ignat.loskutov@gmail.com>
2014-09-24 21:01:02 +0400

    Some initial UI created

--- app/src/main/java/ru/ifmo/md/lesson3/MainActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson3/MainActivity.java
@@ -2,8 +2,10 @@ package ru.ifmo.md.lesson3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +14,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+
     }
 
 }
