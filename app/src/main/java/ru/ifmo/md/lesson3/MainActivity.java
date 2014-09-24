5d686394047f7045ee7403d58f148cef1fd3ac5a
Ignat Loskutov <ignat.loskutov@gmail.com>
2014-09-25 00:53:53 +0400

    A little code cleanup

--- app/src/main/java/ru/ifmo/md/lesson3/MainActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson3/MainActivity.java
@@ -37,17 +37,13 @@ public class MainActivity extends Activity {
 
     class TranslationTask extends AsyncTask<String, Void, Void> {
         @Override
-        protected Void doInBackground(String... urls) {
+        protected Void doInBackground(String... word) {
             try {
-                String url = apiPrefix + URLEncoder.encode(urls[0], "utf-8");
+                String url = apiPrefix + URLEncoder.encode(word[0], "utf-8");
                 DefaultHttpClient httpClient = new DefaultHttpClient();
-                HttpEntity httpEntity = null;
-                HttpResponse httpResponse = null;
-                HttpGet httpGet = new HttpGet(url);
-                httpResponse = httpClient.execute(httpGet);
-                httpEntity = httpResponse.getEntity();
-                String answer = EntityUtils.toString(httpEntity);
-                JSONObject jsonObject = new JSONObject(answer);
+                HttpResponse httpResponse = httpClient.execute(new HttpGet(url));
+                String response = EntityUtils.toString(httpResponse.getEntity());
+                JSONObject jsonObject = new JSONObject(response);
                 Log.i("lal", jsonObject.getJSONArray("text").getString(0));
             } catch (Exception e) {
                 e.printStackTrace();
