d954e9d3a2f7daa26f7011d08e8a5819d106cf8c
Viktor <viteg34@yandex.ru>
2014-09-26 22:15:00 +0400

    -find some links -download one image

--- app/src/main/java/com/example/vi34/lesson3/ImageDownloader.java
+++ app/src/main/java/com/example/vi34/lesson3/ImageDownloader.java
@@ -1,10 +1,19 @@
 package com.example.vi34.lesson3;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
+
 
 /**
  * Created by vi34 on 26.09.14.
@@ -12,15 +21,59 @@ import java.net.URL;
 public class ImageDownloader {
 
     Bitmap bmp = null;
-    public Bitmap search(String querry){
 
-        new DownloadImageTask().execute("s");
+    public Bitmap search(String querry) {
+
+        new DownloadImageTask().execute(querry);
         return bmp;
     }
 
     private class DownloadImageTask extends AsyncTask<String, Integer, Bitmap> {
+
+        protected void onPreExecute() {
+        }
+
         protected Bitmap doInBackground(String... querry) {
             Bitmap bmp = null;
+
+            try {
+
+                URL url = new URL("https://ajax.googleapis.com/ajax/services/search/images?" +
+                        "v=1.0&q=nyan%20cat&userip=INSERT-USER-IP");
+
+
+                URLConnection connection = url.openConnection();
+                connection.addRequestProperty("Referer", "app");
+
+                String line;
+                StringBuilder builder = new StringBuilder();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                }
+
+
+                JSONObject json = new JSONObject(builder.toString());
+                Log.i("ASYNC", builder.toString());
+                JSONArray resultArray = json.getJSONObject("responseData").getJSONArray("results");
+                String[] imageLinks = new String[resultArray.length()];
+
+                for (int i = 0; i < resultArray.length(); i++) {
+                    json = resultArray.getJSONObject(i);
+                    imageLinks[i] = json.getString("url");
+                }
+                Log.i("ASYNC", imageLinks[0] + " " + resultArray.length() + " results");
+
+                url = new URL(imageLinks[0]);
+                connection = url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                return BitmapFactory.decodeStream(input);
+
+            } catch (Exception e) {
+                Log.i("ASYNC", "AAA, SOMETHING GOES WRONG!!!!! AAAA");
+            }
 
             publishProgress(1);
 
@@ -29,7 +82,7 @@ public class ImageDownloader {
         }
 
         protected void onProgressUpdate(Integer... progress) {
-           Log.i("ASYNC", "Update OK");
+            Log.i("ASYNC", "Update OK");
         }
 
         protected void onPostExecute(Bitmap result) {
