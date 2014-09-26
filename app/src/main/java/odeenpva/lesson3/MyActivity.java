4d38997fc545cf8595ff6b19195fa2c961a6f136
EvgenyZamyatin <jeka_zamyatin@mail.ru>
2014-09-26 21:22:39 +0400

    PictureLoader added

--- app/src/main/java/odeenpva/lesson3/MyActivity.java
+++
@@ -1,7 +0,0 @@
-package odeenpva.lesson3;
-
-/**
- * Created by Женя on 25.09.2014.
- */
-public class MyActivity {
-}
---
+++ app/src/main/java/odeenpva/lesson3/PictureLoader.java
@@ -0,0 +1,7 @@
+package odeenpva.lesson3;
+
+/**
+ * Created by Женя on 26.09.2014.
+ */
+public class PictureLoader {
+}
