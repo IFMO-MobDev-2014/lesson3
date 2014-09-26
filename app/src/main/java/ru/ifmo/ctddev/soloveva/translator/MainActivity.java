71a9888a4d5c98aa95baa03ad487b1ed23e0a5d9
s-sladen <m.luscinia@gmail.com>
2014-09-26 22:39:32 +0400

    init

---
+++ app/src/main/java/ru/ifmo/ctddev/soloveva/translator/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.ifmo.ctddev.soloveva.translator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
