472b915ad6ddbf56a389189479820301fe0c0ef7
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2014-09-27 14:57:31 +0400

    add `ResultActivity`

--- app/src/main/java/com/abrafcm/translator/translator/TranslateActivity.java
+++ app/src/main/java/com/abrafcm/translator/translator/TranslateActivity.java
@@ -1,19 +1,47 @@
 package com.abrafcm.translator.translator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class TranslateActivity extends Activity {
+    private static final String TAG = "TranslateActivity";
+
+    private Button mTranslateButton;
+    private EditText mTranslateEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_translate);
+
+        initViews();
     }
 
+    private void initViews() {
+        mTranslateButton = (Button) findViewById(R.id.button_translate);
+        mTranslateEdit = (EditText) findViewById(R.id.edit_translate);
+
+        mTranslateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String toTranslate = mTranslateEdit.getText().toString();
+                startResultActivity(toTranslate);
+            }
+        });
+    }
+
+    private void startResultActivity(String translate) {
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra(Helper.TRANSLATE, translate);
+        startActivity(intent);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
