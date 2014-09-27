8b3908dff3a2de8cc6f01b7e3094e9208868694d
Evgeny Vinogradov <zhenyavinogradov@gmail.com>
2014-09-27 10:55:59 +0400

    added ShowImage

---
+++ app/src/main/java/ru/ifmo/md/lesson3/ImageAdapter.java
@@ -0,0 +1,59 @@
+package ru.ifmo.md.lesson3;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<Drawable> pictures = new ArrayList<Drawable>();
+
+    public ImageAdapter(Context c) {
+        context = c;
+
+    }
+
+    public int getCount() {
+        return pictures.size();
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public void add(Drawable picture) {
+        pictures.add(picture);
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            imageView = new ImageView(context);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        Drawable picture = pictures.get(position);
+        imageView.setImageDrawable(picture);
+        return imageView;
+    }
+
+}
--- app/src/main/java/ru/ifmo/md/lesson3/ResultActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson3/ResultActivity.java
@@ -2,13 +2,21 @@ package ru.ifmo.md.lesson3;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.GridView;
 import android.widget.TextView;
+
+import java.io.InputStream;
+import java.net.URL;
 
 public class ResultActivity extends Activity {
 
     TextView textView;
+    GridView gridView;
+    ImageAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +24,14 @@ public class ResultActivity extends Activity {
         setContentView(R.layout.activity_result);
         textView = (TextView) findViewById(R.id.textView);
 
+        gridView = (GridView) findViewById(R.id.gridView);
+        adapter = new ImageAdapter(this);
+        gridView.setAdapter(adapter);
+
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_QUERY);
         new FindTranslationTask().execute(message);
+        new FindImagesTask().execute(message);
     }
 
     private class FindTranslationTask extends AsyncTask<String, Void, String> {
@@ -36,35 +49,40 @@ public class ResultActivity extends Activity {
             textView.setText(result);
         }
     }
-/*
 
-
-    private class FindImageTask extends AsyncTask<String, Void, String> {
-        protected String doInBackground(String... strings) {
-            String urlTemplate = "https://wiki.videolan.org/images/Caption_%n.png";
+    private class FindImagesTask extends AsyncTask<String, Void, Void> {
+        protected Void doInBackground(String... strings) {
+            String urlTemplate = "http://www.joomlaworks.net/images/demos/galleries/abstract/%d.jpg"; // dummy
             try {
-                for (int i=0; i<10; i++) {
-                    String urlString = String.format(urlTemplate, key, lang, strings[0]);
-                    URL url = new URL(urlString);
-                    URLConnection conn = url.openConnection();
-
-                    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-                    DocumentBuilder builder = factory.newDocumentBuilder();
-                    Document doc = builder.parse(conn.getInputStream());
-
-                    Node node = doc.getElementsByTagName("text").item(0);
-                    String result = node.getTextContent();
+                for (int i=1; i<=10; i++) {
+                    URL url = new URL(String.format(urlTemplate, i));
+                    new ShowImageTask().execute(url);
                 }
-                return result;
             } catch (Exception e) {
                 Log.e("error", "", e);
-                return e.toString();
             }
+            return null;
+        }
+    }
+
+
+    private class ShowImageTask extends AsyncTask<URL, Void, Drawable> {
+        protected Drawable doInBackground(URL... urls) {
+            URL url = urls[0];
+            try {
+                InputStream content = (InputStream)url.getContent();
+                Drawable picture = Drawable.createFromStream(content , "src");
+                return picture;
+            } catch (Exception e) {
+                Log.e("error", "", e);
+            }
+            return null;
         }
 
-        protected void onPostExecute(String[] result) {
-            textView.setText(result[0]);
+        protected void onPostExecute(Drawable picture) {
+            adapter.add(picture);
+            adapter.notifyDataSetChanged();
         }
-    }*/
+    }
 }
 
