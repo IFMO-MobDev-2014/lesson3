53ad3cc3fe213a6e82d7316f3330ed5c656b41a8
Sergey Patrikeev <serejke.best@gmail.com>
2014-09-27 19:56:48 +0400

    Initial commit. Project created

---
+++ Translator/app/src/main/java/ru/ifmo/md/lesson3/patrikeev_shah/translator/ResultActivity.java
@@ -0,0 +1,10 @@
+package ru.ifmo.md.lesson3.patrikeev_shah.translator;
+
+import android.app.Activity;
+
+/**
+ * Created by sergey on 27.09.14.
+ */
+public class ResultActivity extends Activity {
+
+}
