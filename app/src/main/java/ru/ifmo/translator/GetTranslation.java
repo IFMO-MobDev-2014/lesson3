e4fb749200b9158ef9742a3eaddca21b2110c52d
mariashko <bisyarinamariashka@gmail.com>
2014-09-27 20:46:14 +0000

    Added translator prototype

---
+++ app/src/main/java/ru/ifmo/translator/GetTranslation.java
@@ -0,0 +1,57 @@
+package ru.ifmo.translator;
+
+import android.os.AsyncTask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.util.Arrays;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
+/**
+ * @author Zakhar Voit (zakharvoit@gmail.com)
+ */
+public class GetTranslation extends AsyncTask <String, Void, String> {
+
+    public String response;
+
+    @Override
+    protected String doInBackground(String... params) {
+        try {
+            HttpsURLConnection connection = ConnectionFactory.getHTTPS(Arrays.toString(params));
+            connection.disconnect();
+            BufferedInputStream in = new BufferedInputStream(connection.getInputStream());
+            response = readLine(in);
+            JSONObject json = new JSONObject(response);
+            response = json.getString("text");
+            response = response.substring(3, response.length() - 3);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+
+    private StringBuilder out = new StringBuilder();
+    private String readLine(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+        out.append(br.readLine());
+        br.close();
+        return out.toString();
+    }
+}
+
--- app/src/main/java/ru/ifmo/translator/TranslationLoader.java
+++ app/src/main/java/ru/ifmo/translator/TranslationLoader.java
@@ -1,10 +1,20 @@
 package ru.ifmo.translator;
 
+import java.util.concurrent.ExecutionException;
+
 /**
  * @author Zakhar Voit (zakharvoit@gmail.com)
  */
 public class TranslationLoader {
-    String translate(String query) {
-        return "Daddy";
+    static String translate(String query) {
+        String result = "Translation error";
+        try {
+            result = new GetTranslation().execute(query).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
