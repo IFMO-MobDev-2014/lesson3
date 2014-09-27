b975b44408b027b8707a5f95513f0ee1ad78e3df
Snopi <snopiashe4ka@gmail.com>
2014-09-27 21:36:01 +0400

    added some classes

---
+++ app/src/main/java/com/example/home/lesson3/PicturesDownloader.java
@@ -0,0 +1,76 @@
+package com.example.home.lesson3;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Base64;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by Snopi on 27.09.2014.
+ */
+public class PicturesDownloader extends AsyncTask<String, Void, Bitmap[]> {
+    private static final String apiKey = "PXaJFgnFYAKfNdr3Gir/NfxjKVEOvt4kkQWAd9KMusM=";
+    private static String readStream(InputStream data) {
+        String ans = "";
+        try {
+            StringBuilder inStr = new StringBuilder();
+            BufferedReader br = new BufferedReader(new InputStreamReader(data));
+            String line = br.readLine();
+            while (line != null) {
+                inStr.append(line);
+                inStr.append('\n');
+                line = br.readLine();
+            }
+            ans = inStr.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ans;
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap[] bitmaps) {
+        super.onPostExecute(bitmaps);
+    }
+
+    @Override
+    protected Bitmap[] doInBackground(String... strings) {
+        int numOfPictures = 5;
+        Bitmap[] resImages = new Bitmap[numOfPictures];
+        String bingUrl = "https://api.datamarket.azure.com/Bing/Search/Image?$format=json&$top=" +
+                numOfPictures + "&Query=%27" + strings[0].replaceAll(" ", "+") + "%27";
+        byte[] apiKeyBytes = Base64.encode((":" + apiKey).getBytes(), Base64.DEFAULT);
+        String apiEnc = new String(apiKeyBytes);
+        try {
+            URL url = new URL(bingUrl);
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setRequestProperty("Authorization", "Basic " + apiEnc);
+            JSONObject response = new JSONObject(readStream(urlConnection.getInputStream()));
+            JSONArray results = response.getJSONObject("d").getJSONArray("results");
+            String picUrl;
+            for (int i = 0; i < results.length(); i++) {
+                picUrl = results.getJSONObject(i).getJSONObject("Thumbnail").getString("MediaUrl");
+                resImages[i] = BitmapFactory.decodeStream((InputStream) new URL(picUrl).getContent());
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return resImages;
+    }
+}
---
+++ app/src/main/java/com/example/home/lesson3/YandexTranslator.java
@@ -0,0 +1,51 @@
+package com.example.home.lesson3;
+
+import android.os.AsyncTask;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+/**
+ * Created by User on 27.09.2014.
+ */
+public class YandexTranslator extends AsyncTask<String, Void, String> {
+    private static final String apiKey = "trnsl.1.1.20140924T185319Z.988cfba8926139f0" +
+            ".cd1a85b0a94a070c8f04b7c8fd7ac83aa6542a56";
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        String url = "https://translate.yandex.net/api/v1.5/tr/translate?" +
+                "key=" + apiKey +
+                "&text=" + strings[0] + "&lang=en-ru";
+        try {
+            Document xmlResponse = DocumentBuilderFactory.newInstance()
+                    .newDocumentBuilder().parse((InputStream) new URL(url).getContent());
+            return xmlResponse.getDocumentElement().
+                    getElementsByTagName("text").
+                    item(0).
+                    getFirstChild().
+                    getNodeValue();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
