09d6cabf377ca939dfdf36def7d8d6de61988a26
Cool Girl <evgeniya-katunina@yandex.ru>
2014-09-28 22:50:51 +0300

    All is done.

--- app/src/main/java/ru/ifmo/ctddev/katunina/translator/TranslateActivity.java
+++ app/src/main/java/ru/ifmo/ctddev/katunina/translator/TranslateActivity.java
@@ -4,6 +4,7 @@ package ru.ifmo.ctddev.katunina.translator;
  * Created by Евгения on 28.09.2014.
  */
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,7 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.List;
 
+import ru.ifmo.ctddev.soloveva.translator.MainActivity;
 import ru.ifmo.ctddev.soloveva.translator.R;
 
 
@@ -48,33 +50,37 @@ public class TranslateActivity extends Activity {
     ProgressBar pbTranslating;
 
     GsonGetter<ResponseContainer> currentGetter;
+    public static final  String KEY_TO_WORD = "wordToTranslate";
+    public static final  String KEY_TO_SB = "sbKey";
 
     public void btnTranslateOnClick(View view) throws UnsupportedEncodingException {
         String apiKey = "trnsl.1.1.20140927T181312Z.f77e1f526c98445d.6cf115583bde2a4771c08e22c7615b0fc84b9a42";
         String direction = "en-ru";
-        String word = URLEncoder.encode(edtInput.getText().toString(), "UTF-8");
+        final String word = URLEncoder.encode(edtInput.getText().toString(), "UTF-8");
+        if (word.length() == 0)
+            return;
 
         String uri = String.format("https://translate.yandex.net/api/v1.5/tr.json/translate?key=%s&lang=%s&text=%s",
                 apiKey,
                 direction,
                 word
         );
-
         currentGetter = new GsonGetter<>(ResponseContainer.class);
         currentGetter.get(uri, new GsonGetter.Callback<ResponseContainer>() {
             @Override
             public void onComplete(GsonGetter<ResponseContainer> sender, ResponseContainer result) {
-                if (sender != currentGetter)
-                    return;
-                pbTranslating.setVisibility(View.GONE);
-                if (result == null)
-                    return;
-                for (String line : result.text)
-                    tvResult.append(line + "\n");
+                Intent intent = new Intent(TranslateActivity.this, MainActivity.class);
+                StringBuilder sb = new StringBuilder();
+                if (result.text!=null) {
+                    for (String string : result.text) {
+                        sb.append(string);
+                        sb.append("\n");
+                    }
+                    intent.putExtra(KEY_TO_WORD, word);
+                    intent.putExtra(KEY_TO_SB, sb.toString());
+                    startActivity(intent);
+                }
             }
         });
-
-        tvResult.setText("");
-        pbTranslating.setVisibility(View.VISIBLE);
     }
 }
--- app/src/main/java/ru/ifmo/ctddev/soloveva/translator/MainActivity.java
+++ app/src/main/java/ru/ifmo/ctddev/soloveva/translator/MainActivity.java
@@ -3,25 +3,36 @@ package ru.ifmo.ctddev.soloveva.translator;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.util.List;
 
+import ru.ifmo.ctddev.katunina.translator.TranslateActivity;
 import ru.ifmo.ctddev.soloveva.translator.api.BingImageSearchService;
 
 
 public class MainActivity extends Activity {
     private static final String ACCOUNT_KEY = "PLazKWUyzOiy9LUSy7t4RL/+DNueujtQ7HXri88/t0w";
 
+    private void findViewsByIds() {
+        tvResult = ((TextView) findViewById(R.id.tvResult));
+        lytRoot = ((LinearLayout) findViewById(R.id.lytRoot));
+    }
+
+    TextView tvResult;
+    LinearLayout lytRoot;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ProgressBar progressBar = new ProgressBar(this);
-        progressBar.setIndeterminate(true);
-        setContentView(progressBar);
-        new SearchTask().execute("wolf");
+        setContentView(R.layout.main_activity);
+        findViewsByIds();
+        tvResult.setText(getIntent().getStringExtra(TranslateActivity.KEY_TO_SB));
+        new SearchTask().execute(getIntent().getStringExtra(TranslateActivity.KEY_TO_WORD));
     }
 
     private class SearchTask extends AsyncTask<String, Void, List<? extends ImageData>> {
@@ -43,8 +54,10 @@ public class MainActivity extends Activity {
         @Override
         protected void onPostExecute(List<? extends ImageData> images) {
             ListView view = new ListView(MainActivity.this);
+            view.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT
+                    , ViewGroup.LayoutParams.MATCH_PARENT));
             view.setAdapter(new ImagesAdapter(MainActivity.this, images));
-            setContentView(view);
+            lytRoot.addView(view);
         }
     }
 }
