b09f1f0b505fb17b76f2407c0e3dbbc09d1423fb
lightning95 <aydar.gizatullin@gmail.com>
2014-09-28 16:42:18 +0400

    Translator is used instead of "FakeTranslateProvider"

--- app/src/main/java/com/abrafcm/translator/translator/ResultActivity.java
+++ app/src/main/java/com/abrafcm/translator/translator/ResultActivity.java
@@ -2,24 +2,35 @@ package com.abrafcm.translator.translator;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by Nikita Yaschenko aka Nafanya on 27.09.14.
+ * Modified by Aydar Gizatullin aka lightning95 on 28.09.14.
+ */
 
 public class ResultActivity extends Activity {
     private static final String TAG = "ResultActivity";
 
-    private ITranslateProvider mTranslator;
-    private IImagesProvider mImagesProvider;
     private GridView mGridView;
     private TextView mTranslationTextView;
     private ArrayList<ImageItem> mItems = null;
+    private IImagesProvider mImagesProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +42,21 @@ public class ResultActivity extends Activity {
 
         setupViews();
 
-        mTranslator = new FakeTranslateProvider();
         mImagesProvider = new FakeImagesProvider(this);
         mItems = mImagesProvider.getItems(translateString);
 
-        setupAdapter();
+        String resString = "";
+        Translator translator = new Translator();
+        try {
+            resString = translator.execute(translateString).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
 
-        mTranslationTextView.setText(mTranslator.translate(translateString));
+        setupAdapter();
+        mTranslationTextView.setText(resString);
     }
 
     private void setupViews() {
@@ -53,7 +72,6 @@ public class ResultActivity extends Activity {
         }
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.result, menu);
@@ -67,5 +85,33 @@ public class ResultActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private class Translator extends AsyncTask<String, Void, String> {
+        private static final String API_URL = "https://translate.yandex.net/api/v1.5/tr.json/translate?";
+        private static final String API_KEY = "trnsl.1.1.20140927T210341Z.e8c195ae06daeb13.b1522070ee0c890f7acfc10239c5fe0cb78399e1";
+        private static final String LANGUAGES = "en-ru";
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                String word = params[0];
+
+                URL requestUrl = new URL(API_URL + "key=" + API_KEY + "&text=" + word + "&lang=" + LANGUAGES);
+
+                HttpURLConnection connection = (HttpURLConnection) requestUrl.openConnection();
+                connection.connect();
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(connection.getInputStream()));
+
+                String returned = in.readLine();
+                JSONObject object = new JSONObject(returned);
+                JSONArray c = (JSONArray) object.get("text");
+                return c.getString(0);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
