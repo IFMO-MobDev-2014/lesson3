83e62eb855c28258d9fe4ed0ea3a3e8fa51cd2ff
Ilya I. Lyamkin <ilya.lyamkin@gmail.com>
2014-09-28 17:22:43 +0400

    Added second activity.

--- app/src/main/java/com/cococo/lesson3/result.java
+++ app/src/main/java/com/cococo/lesson3/result.java
@@ -5,42 +5,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.EditText;
 
-public class start_page extends Activity implements OnClickListener {
 
-    Button btnTranslate;
-    EditText editWord;
+public class result extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_page);
-        editWord = (EditText) findViewById(R.id.editWord);
-        btnTranslate = (Button) findViewById(R.id.btnTranslate);
-        btnTranslate.setOnClickListener(this);
+        setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        String word = intent.getStringExtra("word");
+
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnTranslate:
-                Intent intent = new Intent(this, result.class);
-                intent.putExtra("word", editWord.getText().toString());
-                startActivity(intent);
-                break;
-            default:
-                break;
-        }
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.start_page, menu);
+        getMenuInflater().inflate(R.menu.result, menu);
         return true;
     }
 
