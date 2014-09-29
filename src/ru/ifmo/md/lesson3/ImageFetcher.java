e8f3cd2fae8c450cf32aa64448e252ba17d337d1
Korshikov <pk.95@mail.ru>
2014-09-29 19:52:02 +0400

    start picture

--- src/ru/ifmo/md/lesson3/ImageFetcher.java
+++ src/ru/ifmo/md/lesson3/ImageFetcher.java
@@ -1,23 +1,49 @@
 package ru.ifmo.md.lesson3;
 
-import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 //TODO
 public class ImageFetcher {
-    private final Resources res;
-    private final int N;
-    public ImageFetcher(int num, Resources r) {
-        res = r;
-        N = num;
+    private final int pictureCount;
+    private static final String sekretKey = "ad4a117a330cb8d1";
+    private static final String key = "e668a28be78987a1e7c65b60e8d08c3f";
+    private static final String pictureUrl = "https://api.flickr.com/services/rest/";
+
+    public ImageFetcher(int N) {
+        this.pictureCount = N;
     }
 
-    public Bitmap[] fetch(String query) {
-        Bitmap[] imgs = new Bitmap[N];
-        for (int i = 0; i < N; i++) {
-            imgs[i] = BitmapFactory.decodeResource(res, R.drawable.sad);
+
+    //"https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+key+"&text=test&per_page="+pictureCount+"&format=json&nojsoncallback=1"
+
+    public Bitmap[] fetch(String query)  {
+
+        Bitmap [] result = new Bitmap[pictureCount];
+
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost(pictureUrl);
+        List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+        pairs.add(new BasicNameValuePair("method", "flickr.photos.search"));
+        pairs.add(new BasicNameValuePair("api_key", key));
+        pairs.add(new BasicNameValuePair("text", query));
+        pairs.add(new BasicNameValuePair("per_page", Integer.toString(pictureCount)));
+        pairs.add(new BasicNameValuePair("format", "json"));
+        try{
+            post.setEntity(new UrlEncodedFormEntity(pairs));
+        }catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
         }
-        return imgs;
+
+        return result;
     }
 }
