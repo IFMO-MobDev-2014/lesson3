249036c23a8355f0270e4c7ae198c5c800bb828f
Anastasia Maksimovskih <anastasiyamaksimovskih@gmail.com>
2014-09-30 00:13:40 +0400

    +translator

---
+++ Translater/app/src/main/java/homework3/translater/Translator.java
@@ -0,0 +1,60 @@
+package homework3.translater;
+import android.os.AsyncTask;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+/**
+ * Created by Anstanasia Maksimovskih on 29.09.2014.
+ */
+
+
+
+public class Translator extends AsyncTask<String, String, String>{
+    final static String key = "trnsl.1.1.20140929T153338Z.b0695ea50453dbf6.45c30639fe49d07b9a5b4febf8a44fe4985a3670";
+
+
+    protected String doInBackground(String... text) {
+        String link = "https://translate.yandex.net/api/v1.5/tr/translate?" +
+                "key=" + key +
+                "&text=" + text[0] +
+                "&lang=en-ru";
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            URL url = new URL(link);
+            Document doc = db.parse(url.openStream());
+            try {
+                return doc.getElementsByTagName("text").item(0).getTextContent();
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+        return "ERROR";
+    }
+
+
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        //rewrite
+        Main.x = s.subSequence(0, s.length());
+        Main.tv.setText(Main.x);
+    }
+
+
+
+}
