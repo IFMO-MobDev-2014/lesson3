c48cc2a2af970d248c2e000b2bba8600495ad1f1
timur <gamatima>
2014-09-29 23:47:41 +0400

    Such like working

--- Translator/app/src/main/java/team/good/translator/EditActivity.java
+++ Translator/app/src/main/java/team/good/translator/EditActivity.java
@@ -2,65 +2,31 @@ package team.good.translator;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 public class EditActivity extends Activity {
 
-    public static Bitmap[] strToBitmaps(String str) {
-        Bitmap[] bmps = new Bitmap[10];
-        AsyncTask<String, Void, String[]> task1 = new SearchImagesTask().execute(str);
-        String[] urls = new String[10];
-        try {
-            urls = task1.get();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        for (int i = 0; i < 10; i++) {
-            AsyncTask<String, Void, Bitmap> task2 = new DownloadImageTask().execute(urls[i]);
-            try {
-                bmps[i] = task2.get();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return bmps;
-    }
-
-    public static String translate(String text) {
-        AsyncTask<String, Void, String> task = new TranslateTask().execute(text);
-        String tr;
-        try {
-            tr = task.get();
-            return tr;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "translate function was fucked up";
-        }
-    }
+    public EditText wordS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        wordS = (EditText) findViewById(R.id.wordS);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.edit, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
@@ -70,7 +36,8 @@ public class EditActivity extends Activity {
 
     public void translate(View  w) {
         Intent intent = new Intent(this, ResultActivity.class);
-
+        String word = wordS.getText().toString().trim();
+        intent.putExtra("translate", word);
         startActivity(intent);
     }
 }
