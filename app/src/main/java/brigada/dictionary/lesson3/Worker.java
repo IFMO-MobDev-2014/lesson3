352509a4b99a1ad4eee4378c7677f9fc75fb171e
deniska2010 <shvedovdenis1995@gmail.com>
2014-09-29 22:50:44 +0400

    searching,downloading and displaying images added

--- app/src/main/java/brigada/dictionary/lesson3/Worker.java
+++ app/src/main/java/brigada/dictionary/lesson3/Worker.java
@@ -3,21 +3,33 @@ package brigada.dictionary.lesson3;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
 
 /**
  * Created by Dmitry on 29.09.2014.
  */
 public class Worker extends Activity {
     TextView textView;
-    RelativeLayout layout;
+    LinearLayout layout;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +41,11 @@ public class Worker extends Activity {
         textView.setTextColor(Color.BLACK);
         Intent intent = getIntent();
         String word = intent.getStringExtra("word");
-        Translater translater = new Translater(word);
-        translater.execute();
+        Translator translator = new Translator(word);
+        ImageDownload pictureFinder = new ImageDownload(word);
+        layout = (LinearLayout) findViewById(R.id.imagesLayout);
+        translator.execute();
+        pictureFinder.execute();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,10 +54,10 @@ public class Worker extends Activity {
         });
     }
 
-    public class Translater extends AsyncTask<Void, Void, String> {
+    public class Translator extends AsyncTask<Void, Void, String> {
         String word;
 
-        Translater(String word) {
+        Translator(String word) {
             this.word = word;
         }
 
@@ -67,5 +82,109 @@ public class Worker extends Activity {
             int b = out.indexOf(']') - 1;
             return out.substring(a, b);
         }
+    }
+
+    public class ImageDownload extends AsyncTask<Void, Void, ArrayList<Drawable>> {
+
+        public Drawable download(String link) throws IOException {
+
+            Drawable out;
+            try {
+                out = Drawable.createFromStream((InputStream) new URL(link).getContent(), "Pic");
+
+            } catch (FileNotFoundException e) {
+
+                out =  Drawable.createFromPath("errorPic");
+            }
+            return out;
+
+        }
+
+        private String linker;
+
+        @Override
+        public ArrayList<Drawable> doInBackground(Void... var) {
+
+            URLConnection contact;
+            ArrayList<String> picture = new ArrayList<String>();
+            String URL = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=";
+
+            try {
+
+                for (int j = 0; j < 3; j++) {
+                    int x;
+
+                    if (j==2){
+                        x=2;
+                    } else {
+                        x=4;
+                    }
+
+                    URL page = new URL(URL + linker + "&start=" + (4 * j));
+                    contact = page.openConnection();
+                    contact.connect();
+
+                    BufferedReader in;
+                    in = new BufferedReader(new InputStreamReader(contact.getInputStream(), "UTF8"));
+
+                    JSONObject link = new JSONObject(in.readLine());
+                    link = link.getJSONObject("responseData");
+                    JSONArray images = link.getJSONArray("results");
+
+                    for (int i = 0; i < x; i++) {
+                        JSONObject current = images.getJSONObject(i);
+                        picture.add(current.getString("url"));
+                    }
+
+                }
+
+                ArrayList<Drawable> album = new ArrayList<Drawable>();
+
+                for (int i = 0; i < 10; i++) {
+                    album.add(download(picture.get(i)));
+                }
+
+                return album;
+
+            } catch (Exception e) {
+
+                return null;
+
+            }
+
+        }
+
+        public ImageDownload(String link) {
+            linker = link;
+        }
+
+        @Override
+        public void onPostExecute(ArrayList<Drawable> album) {
+
+            super.onPostExecute(album);
+
+            if (album == null) {
+
+                TextView mistake = (TextView) findViewById(R.id.errorWithPics);
+                mistake.setTextSize(30);
+                mistake.setTextColor(Color.RED);
+                mistake.setText("Not found");
+
+            } else {
+
+                for (int i = 0; i < 10; i++) {
+
+                    ImageView image = new ImageView(Worker.this);
+                    image.setImageDrawable(album.get(i));
+                    layout.addView(image);
+                    layout.invalidate();
+
+                }
+                textView.setTextSize(Color.BLACK);
+
+            }
+
+        }
+
     }
 }
